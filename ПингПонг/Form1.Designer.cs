0ba6f00c58fb20fb14a639ee44e699dd958c0ae3
VimeR <VimeR@DESKTOP-HACHIMA>
2022-02-23 18:58:00 +0300

    Добавьте файлы проекта.

---
+++ ПингПонг/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace ПингПонг
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.Score = new System.Windows.Forms.Label();
+			this.Spl1 = new System.Windows.Forms.Label();
+			this.Spl2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.Game = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.Game)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// Score
+			// 
+			this.Score.AutoSize = true;
+			this.Score.Cursor = System.Windows.Forms.Cursors.UpArrow;
+			this.Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Score.Location = new System.Drawing.Point(371, 9);
+			this.Score.Name = "Score";
+			this.Score.Size = new System.Drawing.Size(39, 15);
+			this.Score.TabIndex = 1;
+			this.Score.Text = "Score";
+			// 
+			// Spl1
+			// 
+			this.Spl1.AutoSize = true;
+			this.Spl1.Location = new System.Drawing.Point(371, 24);
+			this.Spl1.Name = "Spl1";
+			this.Spl1.Size = new System.Drawing.Size(13, 13);
+			this.Spl1.TabIndex = 2;
+			this.Spl1.Text = "0";
+			// 
+			// Spl2
+			// 
+			this.Spl2.AutoSize = true;
+			this.Spl2.Location = new System.Drawing.Point(397, 24);
+			this.Spl2.Name = "Spl2";
+			this.Spl2.Size = new System.Drawing.Size(13, 13);
+			this.Spl2.TabIndex = 3;
+			this.Spl2.Text = "0";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(400, 40);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Restart";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(309, 40);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Start";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// Game
+			// 
+			this.Game.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Game.Location = new System.Drawing.Point(1, 65);
+			this.Game.Name = "Game";
+			this.Game.Size = new System.Drawing.Size(798, 384);
+			this.Game.TabIndex = 6;
+			this.Game.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Game);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.Spl2);
+			this.Controls.Add(this.Spl1);
+			this.Controls.Add(this.Score);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.Game)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label Spl1;
+        private System.Windows.Forms.Label Spl2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox Game;
+    }
+}
+
---
+++ ПингПонг/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПингПонг
+{
+    public partial class Form1 : Form
+    {
+        Ball ball;
+        static Sprite pl1;
+        static Sprite pl2;
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.Game.Paint += new System.Windows.Forms.PaintEventHandler(panel1_Paint);
+            DoubleBuffered = true;
+            pl1 = new Sprite(30, Game.Height / 2, 3, 40);
+            pl2 = new Sprite(Game.Width - 35, Game.Height / 2, 3, 40);
+            ball = new Ball(Game);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(int keys);
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            if (GetAsyncKeyState((int)Keys.W) != 0) pl1.u = true;
+            else pl1.u = false;
+            if (GetAsyncKeyState((int)Keys.S) != 0) pl1.d = true;
+            else pl1.d = false;
+            if (GetAsyncKeyState((int)Keys.Up) != 0) pl2.u = true;
+            else pl2.u = false;
+            if (GetAsyncKeyState((int)Keys.Down) != 0) pl2.d = true;
+            else pl2.d = false;
+            ball.collis(Game);
+            ball.update();
+            ball.print(g, Game);
+            pl1.collis(Game);
+            pl1.update();
+            pl1.print(g);
+            pl2.collis(Game);
+            pl2.update();
+            pl2.print(g);
+            
+            Game.Invalidate();
+        }
+        class Ball
+        {
+            public double x, y, vx = 0.1, vy = 0.1, h, w, speed = 0.1, jumpforce = 1;
+            double cx, cy;
+            public Ball(PictureBox pb) 
+            {
+                x = pb.Height / 2;
+                y = pb.Height / 2;
+            }
+            public void print(Graphics g,PictureBox pa)
+            {
+                Pen p = new Pen(Color.Red, 2);
+                g.DrawEllipse(p, (float)x, (float)y, (float)10, (float)10);
+
+            }
+            public void update()
+            {
+                x += vx + 0.001;
+                y += vy + 0.0010;
+                cx = x + 5;
+                cy = y + 5;
+            }
+            public void collis(PictureBox ga)
+            {
+                
+                if (y <= ga.Top - 65)
+                {
+                    vy = -vy;
+                }
+                if (x >= ga.Right - 15)
+                {
+                    vx = -vx;
+                }
+                if (y >= ga.Bottom - 75)
+                {
+                    vy = -vy;
+                }
+                if (x <= ga.Left)
+                {
+                    vx = -vx;
+                }
+               if (cx - 5 <= pl1.x + pl1.w && cy <= pl1.y + pl1.h && cy >= pl1.y)
+                {
+                    vx = -vx;
+                }
+                if (cx + 5 >= pl2.x + pl2.w && cy <= pl2.y + pl2.h && cy >= pl2.y)
+                {
+                    vx = -vx;
+                }
+            }
+        }
+
+
+
+        class Sprite
+        {
+            public double x = 0, y = 0, vx = 0, vy = 0.001, h, w, speed = 0.1, jumpforce = 1;
+            public bool u = false, d = false;
+            public Sprite(double x, double y, double w, double h)
+            {
+                this.x = x;
+                this.y = y;
+                this.w = w;
+                this.h = h;
+            }
+            public void update()
+            {
+                if (u == true) up();
+                else stopy();
+                if (d == true) down();
+                x += vx;
+                y += vy;
+            }
+            public void print(Graphics g)
+            {
+                Pen p = new Pen(Color.Red, 2);
+                g.DrawRectangle(p, (float)x, (float)y, (float)w, (float)h);
+
+            }
+            public void up()
+            {
+                vy = -speed;
+            }
+            public void down()
+            {
+                vy = speed;
+            }
+            public void stopy()
+            {
+                vy = 0;
+            }
+            public void collis(PictureBox pb)
+            {
+                if (y <= pb.Top - 65)
+                {
+                    u = false;
+                    vy = 0;
+                }
+                if (y + h >= pb.Bottom - 70)
+                {
+                    d = false;
+                    vy = 0;
+                }
+                //if ((y <= sp.y + h + 1 && y >= sp.y) && (Math.Abs(x - sp.x) < sp.w)) u = false;
+                //if ((y + h >= sp.y - 1 && y <= sp.y + h) && (Math.Abs(x - sp.x) < sp.w))
+                //{
+                //    d = false;
+                //    vy = 0;
+                //}
+            }
+        }
+
+    }
+}
+
